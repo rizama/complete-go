a6ebca1ac982c24cde8e3191be834f119d63c9d3
Sam <rizky.pratama@sicepat.com>
2022-02-25 14:34:04 +0700

    decalaration variables out from func main

--- cards/main.go
+++ cards/main.go
@@ -2,9 +2,11 @@ package main
 
 import "fmt"
 
+var card string
+
 func main() {
 	// var card string = "Hello there"
-	card := "Hello There"
+	// card := "Hello There"
 	card = "Hello Sam"
 
 	fmt.Println(card)
