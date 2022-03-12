94c28678711e107e9b33cd05502ed75df8d3bcd4
Rizky Sam Pratama <rizkysampratama@gmail.com>
2022-03-12 19:55:44 +0700

    little change

--- cards/deck.go
+++ cards/deck.go
@@ -2,12 +2,12 @@ package main
 
 import "fmt"
 
-// Create new Type of 'deck
+// Create new Type of 'deck'
 // which is a slice of strings
 type deck []string
 
 func (d deck) myPrint() {
-	for _, card := range d {
-		fmt.Println(card)
+	for i, card := range d {
+		fmt.Println(card, i)
 	}
 }
