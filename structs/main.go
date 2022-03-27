491f87e0f9e175ef893f48968de3b8039f16e0e4
Rizky Sam Pratama <rizkysampratama@gmail.com>
2022-03-27 21:50:10 +0700

    Declaring Struct

--- structs/main.go
+++ structs/main.go
@@ -1,10 +1,18 @@
 package main
 
+import "fmt"
+
 type person struct {
 	firstName string
 	lastName  string
 }
 
 func main() {
+	//	Declaring way 1
+	sam := person{"Sam", "Pratama"}
+	fmt.Println(sam)
 
+	//	Declaring way 2 -> Recomended
+	sem := person{firstName: "Sem", lastName: "Pratame"}
+	fmt.Println(sem)
 }
