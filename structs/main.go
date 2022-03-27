00b3502edfc787dc7cc8ec904bdaacf0a1e96ac4
Rizky Sam Pratama <rizkysampratama@gmail.com>
2022-03-27 22:03:56 +0700

    Updating Struct

--- structs/main.go
+++ structs/main.go
@@ -10,9 +10,17 @@ type person struct {
 func main() {
 	//	Declaring way 1
 	sam := person{"Sam", "Pratama"}
+	sam.lastName = "Rizama"
 	fmt.Println(sam)
 
 	//	Declaring way 2 -> Recomended
 	sem := person{firstName: "Sem", lastName: "Pratame"}
 	fmt.Println(sem)
+
+	//Updating Struct values
+	var rizky person
+	rizky.firstName = "Rizky"
+	rizky.lastName = "Sam"
+	fmt.Println(rizky)
+	fmt.Printf("%+v", rizky)
 }
