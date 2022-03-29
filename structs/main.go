4da9e08c48bb5bc5ab5bc03130c598179ea8d2a1
Rizky Sam Pratama <rizkysampratama@gmail.com>
2022-03-29 23:05:34 +0700

    Embedding Struct

--- structs/main.go
+++ structs/main.go
@@ -5,22 +5,41 @@ import "fmt"
 type person struct {
 	firstName string
 	lastName  string
+	contact   contactInfo
+}
+
+type contactInfo struct {
+	email   string
+	zipCode int
 }
 
 func main() {
-	//	Declaring way 1
-	sam := person{"Sam", "Pratama"}
-	sam.lastName = "Rizama"
-	fmt.Println(sam)
+	//	Declaring way 1 without embedded another struct
+	//sam := person{"Sam", "Pratama"}
+	//sam.lastName = "Rizama"
+	//fmt.Println(sam)
 
-	//	Declaring way 2 -> Recomended
-	sem := person{firstName: "Sem", lastName: "Pratame"}
-	fmt.Println(sem)
+	//	Declaring way 2 without embedded another struct -> Recomended
+	//sem := person{firstName: "Sem", lastName: "Pratame"}
+	//fmt.Println(sem)
 
-	//Updating Struct values
-	var rizky person
-	rizky.firstName = "Rizky"
-	rizky.lastName = "Sam"
-	fmt.Println(rizky)
-	fmt.Printf("%+v", rizky)
+	//Updating Struct values without embedded another struct
+	//var rizky person
+	//rizky.firstName = "Rizky"
+	//rizky.lastName = "Sam"
+	//fmt.Println(rizky)
+	//fmt.Printf("%+v", rizky)
+
+	sampratama := person{
+		firstName: "SAM",
+		lastName:  "PRATAMA",
+		contact: contactInfo{
+			email:   "sam@gmail.com",
+			zipCode: 12345,
+		},
+	}
+
+	fmt.Printf("%+v", sampratama)
+	println("")
+	fmt.Printf("%v", sampratama.contact.email)
 }
