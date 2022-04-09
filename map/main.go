2e3df269a1cd89d07f7045cec6aeff2c88f67de5
Rizky Sam Pratama <rizkysampratama@gmail.com>
2022-04-09 23:20:29 +0700

    Manipulating Map

--- map/main.go
+++ map/main.go
@@ -3,10 +3,16 @@ package main
 import "fmt"
 
 func main() {
+	//Declare Empty Map:
+	colors2 := make(map[int]string)
+	colors2[1] = "One"
+
+	// Decalare + Initial Map
 	colors := map[string]string{
 		"red":   "#ff0000",
 		"black": "#000000",
 	}
 
 	fmt.Println(colors)
+	fmt.Println(colors2)
 }
