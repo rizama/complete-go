816524525f5343005a772ae0db00048cea634ff7
Rizky Sam Pratama <rizkysampratama@gmail.com>
2022-04-10 11:54:12 +0700

    Iteraring over map

--- map/main.go
+++ map/main.go
@@ -4,15 +4,20 @@ import "fmt"
 
 func main() {
 	//Declare Empty Map:
-	colors2 := make(map[int]string)
-	colors2[1] = "One"
+	//colors2 := make(map[int]string)
+	//colors2[1] = "One"
 
-	// Decalare + Initial Map
+	// Declare + Initial Map
 	colors := map[string]string{
 		"red":   "#ff0000",
 		"black": "#000000",
 	}
 
-	fmt.Println(colors)
-	fmt.Println(colors2)
+	printMap(colors)
+}
+
+func printMap(c map[string]string) {
+	for color, hex := range c {
+		fmt.Println("Hex code for", color, "is", hex)
+	}
 }
